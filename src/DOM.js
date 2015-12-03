88fc4574ac432f1add2192eda388f86db2c36f8b
Sean Hogan <shogun70@gmail.com>
2015-12-03 18:10:15 +1100

    Implement Meeko.URL

--- src/DOM.js
+++ src/DOM.js
@@ -25,6 +25,96 @@ var _ = Meeko.stuff;
 /*
  ### DOM utility functions
  */
+var URL = Meeko.URL = (function() {
+
+// TODO Ideally Meeko.URL is read-only compatible with DOM4 URL
+// NOTE This could use `document.createElement('a').href = url` except DOM is too slow
+
+var URL = function(href, base) {
+	if (!(this instanceof URL)) return new URL(href, base);
+	var baseURL;
+	if (base) baseURL = typeof base === 'string' ? new URL(base) : base;
+	init.call(this, href, baseURL);
+}
+
+var init = function(href, baseURL) {
+	if (baseURL) {
+		href = baseURL.resolve(href);
+		_.assign(this, new URL(href));
+	}
+	else {
+		var url = parse(href);
+		for (var key in url) this[key] = url[key]; // _.assign(this, url);
+		enhance(this);
+	}
+}
+
+var keys = ['source','protocol','hostname','port','pathname','search','hash'];
+var parser = /^([^:\/?#]+:)?(?:\/\/([^:\/?#]*)(?::(\d*))?)?([^?#]*)?(\?[^#]*)?(#.*)?$/;
+
+var parse = window.URL && 'href' in window.URL.prototype ? 
+function(href) {
+	return new window.URL(href);
+} :
+function(href) {
+	href = href.trim();
+	var m = parser.exec(str);
+	var url = {};
+	for (var n=keys.length, i=0; i<n; i++) url[keys[i]] = m[i] || '';
+	return url;
+}
+
+function enhance(url) {
+	url.protocol = _.lc(url.protocol);
+	url.supportsResolve = /^(http|https|ftp|file):$/i.test(url.protocol);
+	if (!url.supportsResolve) return;
+	if (url.hostname) url.hostname = _.lc(url.hostname);
+	if (!url.host) {
+		url.host = url.hostname;
+		if (url.port) url.host += ':' + url.port;
+	}
+	if (!url.origin) url.origin = url.protocol + '//' + url.host;
+	if (!url.pathname) url.pathname = '/';
+	var pathParts = url.pathname.split('/'); // creates an array of at least 2 strings with the first string empty: ['', ...]
+	pathParts.shift(); // leaves an array of at least 1 string [...]
+	url.filename = pathParts.pop(); // filename could be ''
+	url.basepath = pathParts.length ? '/' + pathParts.join('/') + '/' : '/'; // either '/rel-path-prepended-by-slash/' or '/'
+	url.base = url.origin + url.basepath;
+	url.nosearch = url.origin + url.pathname;
+	url.nohash = url.nosearch + url.search;
+	url.href = url.nohash + url.hash;
+	url.toString = function() { return url.href; }
+};
+
+URL.prototype.resolve = function resolve(relHref) {
+	relHref = relHref.trim();
+	if (!this.supportsResolve) return relHref;
+	var substr1 = relHref.charAt(0), substr2 = relHref.substr(0,2);
+	var absHref =
+		/^[a-zA-Z0-9-]+:/.test(relHref) ? relHref :
+		substr2 == '//' ? this.protocol + relHref :
+		substr1 == '/' ? this.origin + relHref :
+		substr1 == '?' ? this.nosearch + relHref :
+		substr1 == '#' ? this.nohash + relHref :
+		substr1 != '.' ? this.base + relHref :
+		substr2 == './' ? this.base + relHref.replace('./', '') :
+		(function() {
+			var myRel = relHref;
+			var myDir = this.basepath;
+			while (myRel.substr(0,3) == '../') {
+				myRel = myRel.replace('../', '');
+				myDir = myDir.replace(/[^\/]+\/$/, '');
+			}
+			return this.origin + myDir + myRel;
+		}).call(this);
+	return absHref;
+}
+
+
+return URL;
+
+})();
+
 
 var DOM = Meeko.DOM = (function() {
 
